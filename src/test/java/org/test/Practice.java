8c0ce4893fdd89f0c6e3691f65d7255d60aa6bce
sugansmilely05 <sugansmilely05@gmail.com>
2022-04-20 18:35:16 -0700

    Kamal Code

--- src/test/java/org/test/Practice.java
+++ src/test/java/org/test/Practice.java
@@ -3,6 +3,8 @@ package org.test;
 public class Practice {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("compare");
+		System.out.println("throw compile error");
 	}
 
 }
