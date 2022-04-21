b3e707732d9a68f4441221d394b18295adcbd28e
sugansmilely05 <sugansmilely05@gmail.com>
2022-04-20 18:27:48 -0700

    sugan code

--- src/test/java/org/test/Practice.java
+++ src/test/java/org/test/Practice.java
@@ -3,6 +3,8 @@ package org.test;
 public class Practice {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test1");
+		System.out.println("test 2");
 	}
 
 }
